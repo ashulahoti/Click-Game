8797ba8dbdfeb8609ecdcd0880286e75b5b6258f
ashulahoti <ashulahoti@gmail.com>
2015-10-12 23:18:28 +0530

    Create server.js
    this is used to create serverin node.js and it will render the 2 html pages.

---
+++ server.js
@@ -0,0 +1,17 @@
+var http = require('http');
+var express = require('express');
+var path = require('path');
+app = express();
+
+app.use('/GAME',function(req,res){
+       res.sendFile(__dirname + '/final.html');
+})
+
+app.use(express.static(path.join(__dirname, '/public')));
+
+app.use('/',function(req,res){
+	res.sendFile(__dirname + '/index.html');
+})
+
+http.createServer(app).listen(3000);
+console.log("server running on port 3000");
